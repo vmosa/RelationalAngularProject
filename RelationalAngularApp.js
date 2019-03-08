eb00c399ad067efc6d4e8efe210527b4c99fc9be
vmosa <vmosa@users.noreply.github.com>
2019-03-08 09:04:12 +0200

    Add files via upload

---
+++ RelationalAngularApp.js
@@ -0,0 +1,9 @@
+var app=angular.module('RelationalAngularApp',[]);
+
+app.controller('MainAppController',['$http', '$scope',
+function($http, $scope){
+	$scope.toDo=['First task to do', 'second task to do'];
+	
+	
+	
+}]);
