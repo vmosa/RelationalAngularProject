49977528e9931a8c718bd6113fbb7b245869d75e
vmosa <vmosa@users.noreply.github.com>
2019-03-09 23:26:31 +0200

    Update RelationalAngularApp.js

--- RelationalAngularApp.js
+++ RelationalAngularApp.js
@@ -2,7 +2,15 @@ var app=angular.module('RelationalAngularApp',[]);
 
 app.controller('MainAppController',['$http', '$scope',
 function($http, $scope){
-	$scope.toDo=['First task to do', 'second task to do'];
+	//$scope.toDo=['First task to do', 'second task to do'];
+	$scope.toDo='';
+	$http.get('https://bee1f3cf-4500-40a0-8cb4-0449b3c7d633.mock.pstmn.io/todos')
+	.then((response)=>{
+		$scope.toDo=response;
+	},(response)=>{
+		console.error(response);
+	}
+	);
 	
 	
 	
