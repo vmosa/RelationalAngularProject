37add84e7c13e0a96df5891a302544f466c1fc58
vmosa <vmosa@users.noreply.github.com>
2019-03-10 19:46:15 +0200

    Update app and add service
    * Update RelationalAngularApp
    * Add MainAppHttpService

---
+++ MainAppHttpService.js
@@ -0,0 +1,7 @@
+var app=angular.module('MainAppServiceModule',[]);
+app.factory('MainAppHttpService',['$http', '$scope',
+function($http, url){
+    $http.get()
+
+
+}]);
--- RelationalAngularApp.js
+++ RelationalAngularApp.js
@@ -4,7 +4,8 @@ app.controller('MainAppController',['$http', '$scope',
 function($http, $scope){
 	//$scope.toDo=['First task to do', 'second task to do'];
 	$scope.toDo='';
-	$http.get('https://bee1f3cf-4500-40a0-8cb4-0449b3c7d633.mock.pstmn.io/todos')
+	var URL='https://bee1f3cf-4500-40a0-8cb4-0449b3c7d633.mock.pstmn.io/todos';
+	$http.get(URL);
 	.then((response)=>{
 		$scope.toDo=response.data;
 	},(response)=>{
